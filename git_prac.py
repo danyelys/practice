2bb242c62bdcba12244537c7855d405ba0770a2b
Danyel Chua <e0563833@u.nus.edu>
2024-07-27 20:46:40 +0800

    read_pdf func

--- git_prac.py
+++ git_prac.py
@@ -1,2 +1,16 @@
 import fitz
 import pandas
+import docx
+from collections import defaultdict
+
+pdf_path = r"C:\Users\danyel\Downloads\Resume_danyel_final_final.pdf"
+
+def read_pdf(pdf):
+    #content = fitz.open(stream = pdf, filetype = "pdf")
+    content = fitz.open(pdf, filetype = "pdf")
+    return "".join([page.get_text(sort = True) for page in content])
+
+test_pdf = read_pdf(pdf_path)
+
+
+print(test_pdf)
